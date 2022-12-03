ebe5d91b3a06c278fa028b3e866e5cbb447b5db0
dougfort <doug.fort@gmail.com>
2022-12-03 10:48:39 -0500

    Day 3 part 2

--- advent03.rkt
+++ advent03.rkt
@@ -7,10 +7,30 @@
   (let-values ([(lhs rhs) (split-at l (quotient (length l) 2))])
     (first (set->list (set-intersect (list->set lhs) (list->set rhs))))))
 
-(define (sum-of-priorities d)
+(define (sum-of-shared-item-priorities d)
   (for/sum ([i (in-list d)])
     (priority (shared-item i))))
 
-(sum-of-priorities test-data)
-(sum-of-priorities data)
+(sum-of-shared-item-priorities test-data)
+(sum-of-shared-item-priorities data)
 
+(define (find-badge-in lhs)
+  (let ([l (list->set (first lhs))]
+        [m (list->set (second lhs))]
+        [r (list->set (third lhs))])
+    (first (set->list (set-intersect l m r)))))
+
+(define (sum-of-badge-priorities d)
+  (let loop ([acc 0]
+             [lhs (take d 3)]
+             [rhs (drop d 3)])
+    (let ([s (priority (find-badge-in lhs))])
+        (if (empty? rhs)
+            (+ acc s)
+            (loop (+ acc s) (take rhs 3) (drop rhs 3)))))) 
+         
+          
+    
+             
+              
+          
