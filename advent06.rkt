5066cd5b7f27f4770f686cc4396fe8f555345202
dougfort <doug.fort@gmail.com>
2022-12-06 11:42:45 -0500

    add unit tests

--- advent06.rkt
+++ advent06.rkt
@@ -4,6 +4,9 @@
 
 (require "advent06-data.rkt")
 
+(module+ test
+  (require rackunit))
+
 (define (find-unique-characters size data)
   (define buffer ((compose1 list->vector string->list string-trim) data))
   (for/or ([i (in-range (vector-length buffer))])
@@ -17,5 +20,18 @@
 (define (find-start-of-packet data)
   (find-unique-characters 4 data))
 
+(module+ test
+  (check-equal? (find-start-of-packet raw-test-data)
+                7)
+  (check-equal? (find-start-of-packet "zcfzfwzzqfrljwzlrfnpqdbhtmscgvjw")
+                11))
+
 (define (find-start-of-message data)
     (find-unique-characters 14 data))
+
+(module+ test
+  (check-equal? (find-start-of-message raw-test-data)
+                19)
+  (check-equal? (find-start-of-message "bvwbjplbgvbhsrlpgdmjqwftvncz")
+                23))
+  
