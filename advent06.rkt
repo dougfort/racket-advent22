3fcee00df61252b4cc5fb82995b7cc89dc471c3f
dougfort <doug.fort@gmail.com>
2022-12-06 09:12:29 -0500

    day 6

---
+++ advent06.rkt
@@ -0,0 +1,21 @@
+#lang racket
+
+;; 
+
+(require "advent06-data.rkt")
+
+(define (find-unique-characters size data)
+  (define buffer (list->vector (string->list (string-trim data))))
+  (for/or ([i (in-range (vector-length buffer))])
+    (let* ([last-pos (+ i size)]
+           [vec (vector-copy buffer i last-pos)])
+      (if (check-duplicates (vector->list vec) #:default #f)
+          #f
+          last-pos))))
+
+
+(define (find-start-of-packet data)
+  (find-unique-characters 4 data))
+
+(define (find-start-of-message data)
+    (find-unique-characters 14 data))
